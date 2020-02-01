5c55de4fe480bc95b02bc5562404f0e0ac62c706
Alex <allercher@edu.aau.at>
2020-02-01 01:28:06 +0100

    working connection to aws

--- cloud/db/test.py
+++ cloud/db/test.py
@@ -1,7 +1,19 @@
 import boto3
+from decimal import Decimal
 
 client = boto3.resource('dynamodb', region_name='us-east-2')
 table = client.Table('Location')
 
-response =  table.scan()['Items']
+table.put_item(
+    Item={
+        "id": "device123",
+        "latitude": Decimal('46.624023'),
+        "longitude": Decimal('14.308882'),
+        "timestamp": 1580503753,
+        "username": "Alexander"
+    }
+)
+
+
+response = table.scan()['Items']
 print(response)
